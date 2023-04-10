4a66d65947e489b07568a55aa71d75af2689c04b
guzuro <guzurogmn@gmail.com>
2023-04-10 22:17:32 +0300

    add validation middleware

---
+++ server/src/middlewares/validation.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { ValidationChain, validationResult } from 'express-validator';
+
+export const validation = (schemas: Array<ValidationChain>): express.RequestHandler => {
+    return async (req, res, next) => {
+
+        await Promise.all(schemas.map(schema => schema.run(req)));
+
+        const validation = validationResult(req)
+
+        if (validation.isEmpty()) return next()
+
+        res.statusCode = 400;
+        res.send({
+            error: validation.array()
+        })
+    }
+}
