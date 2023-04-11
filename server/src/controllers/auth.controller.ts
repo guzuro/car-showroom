7f499ac69bf71ad2a59d44a35007536acb73434a
guzuro <guzurogmn@gmail.com>
2023-04-11 23:22:02 +0300

    auth routes

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { hash, compare } from 'bcrypt';
-import { CreateUserDto } from "../dto/create-user.dto";
-import { saveUser } from "../repositories/user.repository";
-import { TypeORMError } from "typeorm";
-import { errorHandler } from "../errorHandler";
+import { getUserByLogin, saveUser } from "../repositories/user.repository";
+import TypeOrmException from "../exceptions/TypeOrmException";
+import HttpException from "../exceptions/HttpException";
 
 export const registerUserHandler = async (req: Request, res: Response, next: NextFunction) => {
     const { login, email, password } = req.body
@@ -17,9 +16,41 @@ export const registerUserHandler = async (req: Request, res: Response, next: Nex
             password: hashPass
         })
 
-        res.send()
+        res.statusCode = 200;
+        res.send({
+            message: 'User created, log in with your login and password!'
+        })
     } catch (error: any) {
-        return errorHandler(error, res)
+        next(new TypeOrmException(error))
     }
 
+}
+
+export const loginUserHandler = async (req: Request, res: Response, next: NextFunction) => {
+    const { login, password } = req.body
+
+    try {
+        const user = await getUserByLogin(login)
+
+        if (user) {
+            const passwordIsOk = await verifyPassword(password, user.password!)
+
+            if (passwordIsOk) {
+                res.statusCode = 200;
+                delete user.password;
+
+                res.send(user)
+            } else {
+                next(new HttpException(401, 'Wrong password'))
+            }
+        } else {
+            next(new HttpException(401, 'Wrong login'))
+        }
+    } catch (error: any) {
+        next(new TypeOrmException(error))
+    }
+}
+
+const verifyPassword = async (plainPass: string, hashPass: string) => {
+    return compare(plainPass, hashPass)
 }
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,16 +3,15 @@ import { Request, Response } from "express";
 import { createUserSchema } from "../schemas/auth/create-user.schema";
 import { loginUserSchema } from "../schemas/auth/login-user.schema";
 import { validation } from "../middlewares/validation";
-import { registerUserHandler } from "../controllers/auth.controller";
+import { loginUserHandler, registerUserHandler } from "../controllers/auth.controller";
 
 const router = express.Router()
 
 
 router.post('/register', validation(createUserSchema), registerUserHandler)
 
-router.post('/login', validation(loginUserSchema), async (req: Request, res: Response) => {
-    res.send('implement login')
-})
+router.post('/login', validation(loginUserSchema), loginUserHandler)
+
 router.post('/refresh')
 
 
