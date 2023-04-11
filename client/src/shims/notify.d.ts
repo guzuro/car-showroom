4989deae76c27a7d13bcb87721c05f08ebdb0657
guzuro <guzurogmn@gmail.com>
2023-04-12 00:46:12 +0300

    shim

---
+++ client/src/shims/notify.d.ts
@@ -0,0 +1,8 @@
+import type { NotificationApiInjection } from 'naive-ui/es/notification/src/NotificationProvider'
+
+declare global {
+    interface Window {
+        $notify: NotificationApiInjection;
+    }
+
+}
