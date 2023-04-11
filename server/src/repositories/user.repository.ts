9733672722636d419511375a944d7fc08a4f784a
guzuro <guzurogmn@gmail.com>
2023-04-11 23:22:14 +0300

    updated user repository

---
+++ server/src/repositories/user.repository.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../config/data-source";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { LoginUserDto } from "../dto/login-user.dto";
+import { User } from "../entities/user.entity";
+
+const userRepository = AppDataSource.getRepository(User);
+
+export const saveUser = async (newUser: CreateUserDto) => {
+    return await userRepository.save({ ...newUser })
+}
+
+export const getUserByLogin = async (login: string) => {
+    return await userRepository.findOneBy({ login })
+}
