820891db7e00e780c0eb6d9d680509fc97387879
guzuro <guzurogmn@gmail.com>
2023-04-14 00:05:13 +0300

    useless imports

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { Request, Response } from "express";
 import { createUserSchema } from "../schemas/auth/create-user.schema";
 import { loginUserSchema } from "../schemas/auth/login-user.schema";
 import { validation } from "../middlewares/validation";
