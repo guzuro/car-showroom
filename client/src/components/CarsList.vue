ec03a3d457ba300ee988d7d6a453edccb44876f4
guzuro <guzurogmn@gmail.com>
2023-04-19 00:06:41 +0300

    moved carslist item card to dedicated component

--- client/src/components/CarsList.vue
+++ client/src/components/CarsList.vue
@@ -1,22 +1,7 @@
 <template>
     <ul class="cars-list">
-        <li v-for="(car, index) in cars" :key="index">
-            <n-card class="cars-list__card" :title="car.model" :header-style="{
-                    textTransform: 'capitalize'
-                }">
-                <template #header-extra>
-                    {{ car.make }}
-                </template>
-                <template #cover>
-                    <div class="cars-list__image">
-                        <n-icon :color="warningColor" class="cars-list__bookmark" size="30" @click="addToWishList(car)">
-                            <bookmark />
-                        </n-icon>
-                        <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKeSfXSGkhfHh3OrZ0LbggDGyT45sz1IRTzQJnzClJ&s">
-                    </div>
-                </template>
-            </n-card>
+        <li v-for="(car, index) in cars" :key="index" @click="addToWishList(car)">
+            <cars-list-item :car="car" />
             {{ car }}
         </li>
     </ul>
@@ -24,16 +9,14 @@
 
 <script lang="ts">
 import { defineComponent, type PropType } from 'vue'
-import { NCard } from 'naive-ui'
-import { Bookmark } from '@vicons/ionicons5';
-import { NIcon } from 'naive-ui';
 import { useThemeVars } from 'naive-ui';
+import { useUserStore } from '../stores/userStore';
+import { useNotification } from '../composables/useNotification';
+import CarsListItem from './CarsListItem.vue'
 
 export default defineComponent({
     components: {
-        NCard,
-        Bookmark,
-        NIcon
+        CarsListItem
     },
     props: {
         cars: {
@@ -43,14 +26,17 @@ export default defineComponent({
     },
 
     setup() {
+        const { user } = useUserStore()
+        const { error } = useNotification()
         const theme = useThemeVars()
         const { warningColor, warningColorHover } = theme.value;
 
         function addToWishList(car: any) {
-            const { model, index } = car
-
-            console.log(car);
-
+            if (!user) {
+                error('Sign in to add to wishlist')
+            } else {
+                const { model, index } = car
+            }
         }
 
         return {
@@ -63,6 +49,8 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+@import '../assets/variables.scss';
+
 .cars-list {
     display: flex;
     flex-wrap: wrap;
@@ -74,38 +62,17 @@ export default defineComponent({
         flex-shrink: 1;
         flex-basis: 100%;
 
-        @media(min-width: 768px) {
+        @media(min-width: $md) {
             flex-basis: 50%;
         }
 
-        @media(min-width: 1024px) {
+        @media(min-width: $lg) {
             flex-basis: 33.33333%;
         }
 
-        @media(min-width: 1280px) {
+        @media(min-width: $xl) {
             flex-basis: 20%;
         }
-    }
-
-    &__card {
-        position: relative;
-    }
-
-    &__bookmark {
-        position: absolute;
-        top: 0;
-        right: 0;
-        cursor: pointer;
-
-        &:hover svg {
-            color: v-bind(warningColorHover) !important;
-        }
-    }
-
-    &__image img {
-        max-width: 200px;
-        margin: 0 auto;
-        display: block;
     }
 }
 </style>
---
+++ client/src/components/CarsListItem.vue
@@ -0,0 +1,75 @@
+<template>
+    <n-card class="cars-list-item" :title="car.model" :header-style="{
+            textTransform: 'capitalize'
+        }">
+        <template #header-extra>
+            {{ car.make }}
+        </template>
+        <template #cover>
+            <div class="cars-list-item__image">
+                <n-icon :color="warningColor" class="cars-list-item__bookmark" size="30" @click="addToWishList(car)">
+                    <bookmark />
+                </n-icon>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKeSfXSGkhfHh3OrZ0LbggDGyT45sz1IRTzQJnzClJ&s">
+            </div>
+        </template>
+    </n-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { NCard } from 'naive-ui'
+import { Bookmark } from '@vicons/ionicons5';
+import { NIcon } from 'naive-ui';
+import { useThemeVars } from 'naive-ui';
+
+export default defineComponent({
+    components: {
+        NCard,
+        Bookmark,
+        NIcon
+    },
+    props: {
+        car: {
+            type: Object,
+            required: true
+        }
+    },
+
+    setup() {
+        const theme = useThemeVars()
+        const { warningColor, warningColorHover } = theme.value;
+
+        return {
+            warningColor,
+            warningColorHover,
+        }
+    }
+})
+</script>
+
+<style scoped lang="scss">
+.cars-list-item {
+    &__card {
+        position: relative;
+    }
+
+    &__bookmark {
+        position: absolute;
+        top: 0;
+        right: 0;
+        cursor: pointer;
+
+        &:hover svg {
+            color: v-bind(warningColorHover) !important;
+        }
+    }
+
+    &__image img {
+        max-width: 200px;
+        margin: 0 auto;
+        display: block;
+    }
+}
+</style>
