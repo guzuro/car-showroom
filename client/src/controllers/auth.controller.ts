392fc0d9d904241c1508c5038b549e6110fc9f7a
guzuro <guzurogmn@gmail.com>
2023-04-20 23:46:35 +0300

    removed router from login

--- client/src/controllers/auth.controller.ts
+++ client/src/controllers/auth.controller.ts
@@ -1,11 +1,9 @@
 import AuthApi from '../Api/auth.api';
 import { useUserStore } from '../stores/userStore';
 import { useLoader } from '../composables/useLoader';
-import { useRouter } from 'vue-router';
 import { useNotification } from '../composables/useNotification';
 
 export default function useAuthController() {
-    const { push } = useRouter();
     const { open, close } = useLoader();
     const { saveUser } = useUserStore();
     const { success } = useNotification();
@@ -16,14 +14,15 @@ export default function useAuthController() {
             open()
 
             const { data } = await AuthApi.login(loginData)
+            console.log('lol');
+
             saveUser(data)
             success("Logged in!");
 
-            push({ name: "Catalog" })
+            return data
         } finally {
             close()
         }
-
     }
 
     return {
