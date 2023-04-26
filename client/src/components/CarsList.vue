7a9559382a0f0ddcb7e3cfb2045ec0c03f8db9fc
guzuro <guzurogmn@gmail.com>
2023-04-26 20:18:22 +0300

    bookmark click fix

--- client/src/components/CarsList.vue
+++ client/src/components/CarsList.vue
@@ -1,64 +1,64 @@
 <template>
-    <ul class="cars-list">
-        <li v-for="(car, index) in cars" :key="index" @click="addToWishList(car)">
-            <cars-list-item :car="car" />
-            {{ car }}
-        </li>
+  <ul class="cars-list">
+    <li v-for="(car, index) in cars" :key="index">
+      <cars-list-item :car="car" @bookmark-click="addToWishList" />
+      {{ car }}
+    </li>
 
-        <auth-modal v-model="authModalOpen" />
-    </ul>
+    <auth-modal v-model="authModalOpen" />
+  </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, type PropType } from 'vue'
-import { useThemeVars } from 'naive-ui';
-import { useUserStore } from '../stores/userStore';
-import { useNotification } from '../composables/useNotification';
+import { useThemeVars } from 'naive-ui'
+import { useUserStore } from '../stores/userStore'
+import { useNotification } from '../composables/useNotification'
 import CarsListItem from './CarsListItem.vue'
-import type { CarInfo } from '../types/CarInfo.type';
-import AuthModal from './AuthModal.vue';
-import { storeToRefs } from 'pinia';
+import type { CarInfo } from '../types/CarInfo.type'
+import AuthModal from './AuthModal.vue'
+import { storeToRefs } from 'pinia'
 
 export default defineComponent({
-    components: {
-        CarsListItem,
-        AuthModal
-    },
-    props: {
-        cars: {
-            type: Array as PropType<Array<CarInfo>>,
-            required: true,
-            default: () => []
-        }
-    },
-
-    setup() {
-        const store = useUserStore()
-        const { user } = storeToRefs(store)
-
-        const authModalOpen = ref(false)
-
-        const { error } = useNotification()
-
-        const theme = useThemeVars()
-        const { warningColor, warningColorHover } = theme.value;
-
-        function addToWishList(car: CarInfo) {
-            if (!user.value) {
-                error('Sign in to add to wishlist')
-                authModalOpen.value = true
-            } else {
-                const { model, index } = car
-            }
-        }
-
-        return {
-            warningColor,
-            warningColorHover,
-            addToWishList,
-            authModalOpen
-        }
+  components: {
+    CarsListItem,
+    AuthModal
+  },
+  props: {
+    cars: {
+      type: Array as PropType<Array<CarInfo>>,
+      required: true,
+      default: () => []
     }
+  },
+
+  setup() {
+    const store = useUserStore()
+    const { user } = storeToRefs(store)
+
+    const authModalOpen = ref(false)
+
+    const { error } = useNotification()
+
+    const theme = useThemeVars()
+    const { warningColor, warningColorHover } = theme.value
+
+    function addToWishList(car: CarInfo) {
+      if (!user.value) {
+        error('Sign in to add to wishlist')
+        authModalOpen.value = true
+      } else {
+        const { model, index } = car
+      }
+    }
+
+    return {
+      warningColor,
+      warningColorHover,
+      addToWishList,
+      authModalOpen
+    }
+  }
 })
 </script>
 
@@ -66,27 +66,27 @@ export default defineComponent({
 @import '../assets/variables.scss';
 
 .cars-list {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 10px;
-    justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  justify-content: center;
 
-    li {
-        flex-grow: 0;
-        flex-shrink: 1;
-        flex-basis: 100%;
+  li {
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: 100%;
 
-        @media(min-width: $md) {
-            flex-basis: 50%;
-        }
-
-        @media(min-width: $lg) {
-            flex-basis: 33.33333%;
-        }
-
-        @media(min-width: $xl) {
-            flex-basis: 20%;
-        }
+    @media (min-width: $md) {
+      flex-basis: 50%;
     }
+
+    @media (min-width: $lg) {
+      flex-basis: 33.33333%;
+    }
+
+    @media (min-width: $xl) {
+      flex-basis: 20%;
+    }
+  }
 }
 </style>
--- client/src/components/CarsListItem.vue
+++ client/src/components/CarsListItem.vue
@@ -1,76 +1,91 @@
 <template>
-    <n-card class="cars-list-item" :title="car.model" :header-style="{
-            textTransform: 'capitalize'
-        }">
-        <template #header-extra>
-            {{ car.make }}
-        </template>
-        <template #cover>
-            <div class="cars-list-item__image">
-                <n-icon :color="warningColor" class="cars-list-item__bookmark" size="30">
-                    <bookmark />
-                </n-icon>
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKeSfXSGkhfHh3OrZ0LbggDGyT45sz1IRTzQJnzClJ&s">
-            </div>
-        </template>
-    </n-card>
+  <n-card
+    class="cars-list-item"
+    :title="car.model"
+    :header-style="{
+      textTransform: 'capitalize'
+    }"
+  >
+    <template #header-extra>
+      {{ car.make }}
+    </template>
+    <template #cover>
+      <div class="cars-list-item__image">
+        <n-icon
+          :color="warningColor"
+          class="cars-list-item__bookmark"
+          size="30"
+          @click="onBookmarkClick"
+        >
+          <bookmark />
+        </n-icon>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKeSfXSGkhfHh3OrZ0LbggDGyT45sz1IRTzQJnzClJ&s"
+        />
+      </div>
+    </template>
+  </n-card>
 </template>
 
 <script lang="ts">
 import { defineComponent, type PropType } from 'vue'
 import { NCard } from 'naive-ui'
-import { Bookmark } from '@vicons/ionicons5';
-import { NIcon } from 'naive-ui';
-import { useThemeVars } from 'naive-ui';
-import type { CarInfo } from '../types/CarInfo.type';
+import { Bookmark } from '@vicons/ionicons5'
+import { NIcon } from 'naive-ui'
+import { useThemeVars } from 'naive-ui'
+import type { CarInfo } from '../types/CarInfo.type'
 
 export default defineComponent({
-    components: {
-        NCard,
-        Bookmark,
-        NIcon
-    },
-    props: {
-        car: {
-            type: Object as PropType<CarInfo>,
-            required: true
-        }
-    },
-
-    setup() {
-        const theme = useThemeVars()
-        const { warningColor, warningColorHover } = theme.value;
-
-        return {
-            warningColor,
-            warningColorHover,
-        }
+  components: {
+    NCard,
+    Bookmark,
+    NIcon
+  },
+  props: {
+    car: {
+      type: Object as PropType<CarInfo>,
+      required: true
     }
+  },
+  emits: ['bookmarkClick'],
+  setup(props, { emit }) {
+    const theme = useThemeVars()
+    const { warningColor, warningColorHover } = theme.value
+
+    function onBookmarkClick() {
+      emit('bookmarkClick', props.car)
+    }
+
+    return {
+      warningColor,
+      warningColorHover,
+      onBookmarkClick
+    }
+  }
 })
 </script>
 
 <style scoped lang="scss">
 .cars-list-item {
-    &__card {
-        position: relative;
-    }
+  &__card {
+    position: relative;
+  }
 
-    &__bookmark {
-        position: absolute;
-        top: 0;
-        right: 0;
-        cursor: pointer;
+  &__bookmark {
+    position: absolute;
+    top: 0;
+    right: 0;
+    cursor: pointer;
 
-        &:hover svg {
-            color: v-bind(warningColorHover) !important;
-        }
+    &:hover svg {
+      color: v-bind(warningColorHover) !important;
     }
+  }
 
-    &__image img {
-        max-width: 200px;
-        margin: 0 auto;
-        display: block;
-    }
+  &__image img {
+    max-width: 200px;
+    margin: 0 auto;
+    display: block;
+  }
 }
 </style>
