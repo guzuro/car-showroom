7caf78a87a594e1cb88bc42082ba257a00c0bdca
guzuro <guzurogmn@gmail.com>
2023-05-03 23:05:45 +0300

    updated header

--- client/src/components/LayoutHeaderUser.vue
+++ client/src/components/LayoutHeaderUser.vue
@@ -1,59 +1,77 @@
 <template>
-    <div class="layout-user-header">
-        <div v-if="user">
-            user icon
-        </div>
-        <div v-else class="layout-user-header__auth" @click="gotoAuthPage">
-            Sign in
-            <n-icon :size="30">
-                <LogInOutline />
-            </n-icon>
-        </div>
+  <div class="layout-user-header">
+    <div class="layout-user-header__user" v-if="user">
+      {{ userFullName }}
+      <n-icon :size="30">
+        <PersonCircle />
+      </n-icon>
     </div>
+    <div v-else class="layout-user-header__auth" @click="authModalOpen = true">
+      Sign in
+      <n-icon :size="30">
+        <LogInOutline />
+      </n-icon>
+
+      <auth-modal v-model="authModalOpen" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { LogInOutline } from '@vicons/ionicons5';
-import { NIcon } from 'naive-ui';
-import { defineComponent } from 'vue'
-import { useUserStore } from '../stores/userStore';
-import { useRouter } from 'vue-router';
+import { LogInOutline, PersonCircle } from '@vicons/ionicons5'
+import { NIcon } from 'naive-ui'
+import { defineComponent, ref } from 'vue'
+import { useUserStore } from '../stores/userStore'
+import { useRouter } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import AuthModal from './AuthModal.vue'
 
 export default defineComponent({
-    components: {
-        NIcon,
-        LogInOutline
-    },
-    setup() {
-        const { user } = useUserStore();
-        const { push } = useRouter()
+  components: {
+    NIcon,
+    LogInOutline,
+    PersonCircle,
+    AuthModal
+  },
+  setup() {
+    const store = useUserStore()
+    const { user, userFullName } = storeToRefs(store)
 
-        function gotoAuthPage() {
-            push({
-                name: "SignIn"
-            })
-        }
+    const { push } = useRouter()
 
-        return {
-            user,
-            gotoAuthPage
-        }
+    const authModalOpen = ref(false)
+
+    function gotoAuthPage() {
+      push({
+        name: 'SignIn'
+      })
     }
+
+    return {
+      user,
+      userFullName,
+      gotoAuthPage,
+      authModalOpen
+    }
+  }
 })
 </script>
 
 <style scoped lang="scss">
 .layout-user-header {
-    &__auth {
-        height: 100%;
-        display: flex;
-        align-items: center;
+  &__auth,
+  &__user {
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
 
-
-        &:hover {
-            cursor: pointer;
-            opacity: .7;
-        }
+  &__user,
+  &__auth {
+    &:hover {
+      cursor: pointer;
+      opacity: 0.7;
     }
+  }
 }
 </style>
--- client/src/stores/userStore.ts
+++ client/src/stores/userStore.ts
@@ -1,8 +1,16 @@
 import { defineStore } from "pinia"
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 export const useUserStore = defineStore('user', () => {
-    const user = ref<Object | null>(null)
+    const user = ref<any | null>(null)
+
+    const userFullName = computed(() => {
+        if (user.value.firstName || user.value.lastName) {
+            return `${user.value.firstName || ''} ${user.value.lastName || ''}`
+        }
+
+        return user.value.email
+    })
 
     function saveUser(payload: any) {
         user.value = payload
@@ -10,6 +18,7 @@ export const useUserStore = defineStore('user', () => {
 
     return {
         user,
+        userFullName,
         saveUser
     }
 })
