9d52d9f97071f903957d62076fd2d13bb1ae93ef
guzuro <guzurogmn@gmail.com>
2023-05-06 01:00:59 +0300

    updated typeormexception

--- server/src/exceptions/TypeOrmException.ts
+++ server/src/exceptions/TypeOrmException.ts
@@ -3,8 +3,13 @@ import HttpException from "./HttpException";
 
 export default class TypeOrmException extends HttpException {
     constructor(error: any) {
-        if (error.code === DUPLICATE_KEY_CODE) {
-            super(409, error.detail)
+        if (error.code) {
+
+            if (error.code === DUPLICATE_KEY_CODE) {
+                super(409, error.detail)
+            }
+        } else {
+            super(500, error.message)
         }
     }
 }
