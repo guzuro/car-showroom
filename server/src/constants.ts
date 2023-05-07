401ba35872b25d2ef830da13b5f52d0f3a06e267
guzuro <guzurogmn@gmail.com>
2023-05-07 16:36:36 +0300

    updated typeorm exception

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,1 +1,2 @@
 export const DUPLICATE_KEY_CODE = '23505'
+export const FK_RECORD_NOT_FOUND = '23503'
--- server/src/exceptions/TypeOrmException.ts
+++ server/src/exceptions/TypeOrmException.ts
@@ -1,12 +1,17 @@
-import { DUPLICATE_KEY_CODE } from "../constants";
+import { DUPLICATE_KEY_CODE, FK_RECORD_NOT_FOUND } from "../constants";
 import HttpException from "./HttpException";
 
 export default class TypeOrmException extends HttpException {
     constructor(error: any) {
-        if (error.code) {
+        console.log(error.message);
 
+        if (error.code) {
             if (error.code === DUPLICATE_KEY_CODE) {
                 super(409, error.detail)
+            } else if (error.code == FK_RECORD_NOT_FOUND) {
+                super(404, error.detail)
+            } else {
+                super(500, error.detail)
             }
         } else {
             super(500, error.message)
