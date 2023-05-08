b3eb1846bb3a975a304971d77f24203db1762cd6
guzuro <guzurogmn@gmail.com>
2023-05-08 18:18:06 +0300

    added list counter with route in header

--- client/src/components/LayoutHeader.vue
+++ client/src/components/LayoutHeader.vue
@@ -1,83 +1,90 @@
 <template>
-    <div class="layout-header-wrapper">
-        <div class="layout-header">
-            <div class="layout-header__app-name" @click="gotoMainPage">
-                <img class="layout-header__logo" :src="logo">
-                <div class="layout-header__title header-title">
-                    <span class="header-title__top">
-                        Car
-                    </span>
-                    <span class="header-title__bottom">
-                        Stories
-                    </span>
-                </div>
-            </div>
-            <layout-header-user />
+  <div class="layout-header-wrapper">
+    <div class="layout-header">
+      <div class="layout-header__app-name" @click="gotoMainPage">
+        <img class="layout-header__logo" :src="logo" />
+        <div class="layout-header__title header-title">
+          <span class="header-title__top"> Car </span>
+          <span class="header-title__bottom"> Stories </span>
         </div>
+      </div>
+      <div class="layout-header__actions">
+        <layout-header-wishes />
+        <layout-header-user />
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import LayoutHeaderUser from './LayoutHeaderUser.vue';
+import LayoutHeaderUser from './LayoutHeaderUser.vue'
+import LayoutHeaderWishes from './LayoutHeaderWishes.vue'
 import logo from '../assets/image/logo.svg'
-import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
-    components: {
-        LayoutHeaderUser
-    },
-    setup() {
-        const { push } = useRouter()
+  components: {
+    LayoutHeaderUser,
+    LayoutHeaderWishes
+  },
+  setup() {
+    const { push } = useRouter()
 
-        function gotoMainPage() {
-            push({
-                name: "Catalog"
-            })
-        }
-
-        return {
-            logo,
-            gotoMainPage
-        }
+    function gotoMainPage() {
+      push({
+        name: 'Catalog'
+      })
     }
+
+    return {
+      logo,
+      gotoMainPage
+    }
+  }
 })
 </script>
 
 <style scoped lang="scss">
 .layout-header {
+  display: flex;
+  justify-content: space-between;
+  padding: 25px;
+
+  &__actions {
     display: flex;
-    justify-content: space-between;
-    padding: 25px;
+    align-items: center;
+    gap: 24px;
+  }
 
-    &__app-name {
-        display: flex;
-        align-items: center;
+  &__app-name {
+    display: flex;
+    align-items: center;
 
-        &:hover {
-            opacity: .7;
-            cursor: pointer;
-        }
+    &:hover {
+      opacity: 0.7;
+      cursor: pointer;
     }
+  }
 
-    &__logo {
-        width: 100%;
-        height: 40px;
-    }
+  &__logo {
+    width: 100%;
+    height: 40px;
+  }
 
-    &__title {
-        line-height: 16px;
-    }
+  &__title {
+    line-height: 16px;
+  }
 
-    &__user {
-        display: flex;
-        align-items: c;
-    }
+  &__user {
+    display: flex;
+    align-items: c;
+  }
 
-    .header-title {
-        &__top {
-            text-transform: uppercase;
-        }
+  .header-title {
+    &__top {
+      text-transform: uppercase;
     }
+  }
 }
 </style>
---
+++ client/src/components/LayoutHeaderWishes.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="layout-header-wishes" v-if="wishes.length" @click="push({ name: 'UsersWishes' })">
+    <n-badge :value="wishes.length">
+      <n-icon :size="30">
+        <format-list-bulleted-filled />
+      </n-icon>
+    </n-badge>
+  </div>
+</template>
+
+<script lang="ts">
+import { FormatListBulletedFilled } from '@vicons/material'
+import { NBadge, NIcon } from 'naive-ui'
+import { defineComponent } from 'vue'
+import { useWishlistStore } from '../stores/wishlistStore'
+import { storeToRefs } from 'pinia'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  components: {
+    NIcon,
+    NBadge,
+    FormatListBulletedFilled
+  },
+  setup() {
+    const wishListStore = useWishlistStore()
+    const { wishes } = storeToRefs(wishListStore)
+    const { push } = useRouter()
+
+    return {
+      wishes,
+      push
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.layout-header-wishes {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+}
+</style>
