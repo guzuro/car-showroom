2d50a74cc16b4d8a8c5f255505ca741342774862
guzuro <guzurogmn@gmail.com>
2023-05-08 18:21:40 +0300

    added wishitem endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,11 @@ import { AppDataSource } from './config/data-source';
 import authRouter from './routes/auth.routes';
 import carsRouter from './routes/cars.routes';
 import wishlistRouter from './routes/wishlist.routes';
+import wishRouter from './routes/wish.routes';
 import errorMiddleware from './middlewares/errorMiddleware';
 const cors = require('cors');
 
 dotenv.config();
-
 
 AppDataSource.initialize()
     .then(() => {
@@ -24,6 +24,7 @@ app.use(cors())
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/wishlist', wishlistRouter);
+app.use('/api/wish', wishRouter);
 app.use('/api', carsRouter);
 
 app.get('/', (_: Request, res: Response) => {
