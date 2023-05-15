df3f47f4ef980937bd5c285286c79d9d670df994
guzuro <guzurogmn@gmail.com>
2023-05-15 23:53:34 +0300

    404 page update

--- client/src/views/404-page.vue
+++ client/src/views/404-page.vue
@@ -1,19 +1,41 @@
 <template>
-    <div>
-        not found
-    </div>
+  <div class="not-found-page">
+    <n-result
+      status="404"
+      title="404 Not Found"
+      description="This page doesn`t exists. Try again from the start"
+    >
+      <template #footer>
+        <n-button @click="push({ name: 'Catalog' })">Main page</n-button>
+      </template>
+    </n-result>
+  </div>
 </template>
 
 <script lang="ts">
+import { NResult, NButton } from 'naive-ui'
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
-    setup() {
-
-
-        return {}
+  components: {
+    NResult,
+    NButton
+  },
+  setup() {
+    const { push } = useRouter()
+    return {
+      push
     }
+  }
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.not-found-page {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
