7135cd864363d4fe2e59a61d764e4775a7e29d41
guzuro <guzurogmn@gmail.com>
2025-05-24 19:59:51 +0300

    fix entities

--- server/src/config/data-source.ts
+++ server/src/config/data-source.ts
@@ -1,23 +1,22 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-import dotenv from 'dotenv';
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import dotenv from "dotenv";
 dotenv.config();
 
 const postgresConfig: PostgresConnectionOptions = {
-    type: "postgres",
-    host: process.env.POSTGRES_HOST,
-    port: +process.env.POSTGRES_PORT!,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_NAME,
-}
-
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT!,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_NAME,
+};
 
 export const AppDataSource = new DataSource({
-    ...postgresConfig,
-    synchronize: true,
-    logging: false,
-    entities: ["dist/**/*.entity.js"],
-    migrations: ['src/migrations/**/*{.ts,.js}'],
+  ...postgresConfig,
+  synchronize: true,
+  logging: false,
+  entities: ["src/**/*.entity.{js,ts}"],
+  migrations: ["src/migrations/**/*{.ts,.js}"],
 });
